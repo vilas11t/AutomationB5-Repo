9764ac84103a74270312d811768fecd21ccc9720
punamsawant <sawantpp14@gmail.com>
2022-07-20 20:41:05 +0530

    Added method in SimpleProgram

--- src/javaBasic/SimpleProgram.java
+++ src/javaBasic/SimpleProgram.java
@@ -4,5 +4,11 @@ public class SimpleProgram {
 
 	public static void main(String[] args) {
 		System.out.println("This is simple demo program");
+		myworld();
+		
+	}
+	
+	static void myworld() {
+		System.out.println("First Clone Test");
 	}
 }
