a9073e2bbfd121c7f876b5a5343b7bdf275a55a7
tanmaysalunke24 <tanmaysalunke2@gmail.com>
2022-07-20 23:16:35 +0530

    addedfile loopconcept

---
+++ src/javaBasic/loopconcept.java
@@ -0,0 +1,14 @@
+package javaBasic;
+
+public class SimpleProgram {
+
+	public static void main(String[] args) {
+		System.out.println("This is simple demo program");
+		myworld();
+		
+	}
+	
+	static void myworld() {
+		System.out.println("First Clone Test");
+	}
+}
