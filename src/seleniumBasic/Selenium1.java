5009c7eedfba7a0dbfc2e022636bb4fa1ad03655
vilas11t <vilas373@gmail.com>
2022-07-20 22:50:17 +0530

    Selenium

---
+++ src/seleniumBasic/Selenium1.java
@@ -0,0 +1,31 @@
+package seleniumBasic;
+
+public class Selenium1 {
+	/*
+	 - It is open source automation testing tool (2004)
+	 - we can write a code in selenium using java, python,c#, ruby, perl etc.
+	 - it used only for web based testing.
+	 - Selenium supports like chrome , IE, safari, mozila, edge etc.
+	 - it can be easly integrated with tools like- git, jenkins, jira, testNG etc
+	 
+	 Components
+	 -4
+	 1. Selenium IDE
+	 2. Selenium Grid
+	 3. Selenium RC
+	 4. Selenium Webdriver
+	 
+	 Limitation:
+	 1. Barcode, captcha and OTP based test scenarios are not possible to test using selenium.
+	 2. Prior programming knowledge is required.
+	 3. we can not automate desktop based applications.
+	 4. AS it open source so no vendor support is available.
+	 
+	 
+	 Selenium Webdriver-
+	 
+	 
+	 
+	 */
+
+}
